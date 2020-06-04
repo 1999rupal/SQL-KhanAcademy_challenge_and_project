e6c7b4f2cf4431a158955d25d019924c5ecc8856
1999rupal <41049143+1999rupal@users.noreply.github.com>
2020-06-05 02:34:58 +0530

    Add files via upload

---
+++ Book_list_database.sql
@@ -0,0 +1,12 @@
+/*What are your favorite books? You can make a database table to store 
+them in! In this first step, create a table to store your list of books. 
+It should have columns for id, name, and rating.
+
+*/
+CREATE TABLE books(id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+/*Now, add three of your favorite books into the table.*/
+INSERT INTO books VALUES(1,"Giver",4);
+INSERT INTO books VALUES(2,"The fault in our star",4);
+INSERT INTO books VALUES(3,"ORIGINALS",4);
+SELECT * FROM books;
